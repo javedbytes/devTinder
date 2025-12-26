9e0d741157c907296181e9ef8fb299d096d67c16
Javed Ahmed <javed@Javeds-MacBook-Air.local>
2025-12-26 22:55:26 +0530

    route handler

--- src/app.js
+++ src/app.js
@@ -2,10 +2,15 @@ const express = require ('express');
 
 const app = express();
 
-app.get("/user/:userId/:name/:password/",(req,res) => {
-    console.log(req.params)
-    res.send({firstName:"javed",lastName:"Ahmed"})
-
+app.get("/user",(req,res,next) => {
+    // route handler
+    console.log("handleling the route")
+    // res.send("response")
+    next()
+},(req,res) => {
+    // route handler 2
+     console.log("handleling the route 2")
+    res.send("response 2")
 })
 
 app.listen(3000,() => {
