7eb0c8523500618b9fe52b94de3f8dce6ea577f1
Javed Ahmed <javed@Javeds-MacBook-Air.local>
2025-12-26 22:21:34 +0530

    routing

--- src/app.js
+++ src/app.js
@@ -2,42 +2,11 @@ const express = require ('express');
 
 const app = express();
 
-// app.use("/",(req,res) => {
-//     res.send("hi from home page");
-// })
-
-// order or routing is very important
-
-// app.use("/hello",(req,res) => {
-//     res.send("hello hello hello");
-// })
-
-
-// this will match all the http api method to /test
-app.use("/test",(req,res) => {
-    res.send("hello from server");
-})
-
-// only match get api call
-app.get("/user",(req,res) => {
+app.get("/user/:userId/:name/:password/",(req,res) => {
+    console.log(req.params)
     res.send({firstName:"javed",lastName:"Ahmed"})
 
 })
-
-app.post("/user",(req,res) => {
-    console.log("save data to database")
-    res.send("data succesfully saved to DB")
-
-})
-
-app.delete("/user",(req,res) => {
-    res.send("deleted succesfully")
-
-})
-
-// app.use("/",(req,res) => {
-//     res.send("hi from home page");
-// })
 
 app.listen(3000,() => {
     console.log("server started on port 3000")
