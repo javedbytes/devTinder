54d573ebbbd1582670b1f00692ebc547da53daaf
Javed Ahmed <javed@Javeds-MacBook-Air.local>
2025-12-27 00:11:15 +0530

    error handeling

--- src/app.js
+++ src/app.js
@@ -1,48 +1,28 @@
 const express = require ('express');
 
 const app = express();
-const {adminAuth,userAuth} = require("./middlewares/auth")
 
-// handle auth middleware for all request , GET, POST, DELETE
-// app.use("/admin",(req,res,next) => {
-//     console.log("admin auth checking...")
-//     const token = "xydddz";
-//     const isAdminAuthorized = token === "xyz"
 
-//     if(!isAdminAuthorized){
-//      res.status(401).send("admin is not authorized")
-//     } else {
-//         next()
-//     }
-
-// })
-
-app.get("/user",userAuth,(req,res) => {
+app.get("/getUserData",(req,res) => {
+    // better to use try catch
+    try {
+        
+    } catch (error) {
+        
+    }
+    // logic of DB call to get user data
+    throw new Error("error")
     res.send("user data send")
 })
 
-app.use("/admin",adminAuth)
-
-app.get("/admin/getAllData",(req,res) => {
-    // logc of checking if the request is authenticated 
-
-    // const token = "xyzkk";
-    // const isAdminAuthorized = token === "xyz"
-    // if(isAdminAuthorized){
-    //         res.send("all data send")
-
-    // } else {
-    //     res.status(401).send("admin is not authorized")
-    // }
-    res.send("all data send")
-
+// error should be first parameter
+app.use("/",(error,req, res,next) => {
+ if(error){
+    // log your error
+    res.status(500).send("something went wrong")
+ }
 })
 
-app.get("/admin/deleteUser",(req,res) => {
-    // logc of checking if the request is authenticated 
-
-    res.send("deleted a user")
-})
 
 app.listen(3000,() => {
     console.log("server started on port 3000")
