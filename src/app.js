6b525b3bbe9404c9767cab0b325566e1f8f76b0f
Javed Ahmed <javed@Javeds-MacBook-Air.local>
2025-12-26 23:54:31 +0530

    middleware

--- src/app.js
+++ src/app.js
@@ -1,16 +1,47 @@
 const express = require ('express');
 
 const app = express();
+const {adminAuth,userAuth} = require("./middlewares/auth")
 
-app.get("/user",(req,res,next) => {
-    // route handler
-    console.log("handleling the route")
-    // res.send("response")
-    next()
-},(req,res) => {
-    // route handler 2
-     console.log("handleling the route 2")
-    res.send("response 2")
+// handle auth middleware for all request , GET, POST, DELETE
+// app.use("/admin",(req,res,next) => {
+//     console.log("admin auth checking...")
+//     const token = "xydddz";
+//     const isAdminAuthorized = token === "xyz"
+
+//     if(!isAdminAuthorized){
+//      res.status(401).send("admin is not authorized")
+//     } else {
+//         next()
+//     }
+
+// })
+
+app.get("/user",userAuth,(req,res) => {
+    res.send("user data send")
+})
+
+app.use("/admin",adminAuth)
+
+app.get("/admin/getAllData",(req,res) => {
+    // logc of checking if the request is authenticated 
+
+    // const token = "xyzkk";
+    // const isAdminAuthorized = token === "xyz"
+    // if(isAdminAuthorized){
+    //         res.send("all data send")
+
+    // } else {
+    //     res.status(401).send("admin is not authorized")
+    // }
+    res.send("all data send")
+
+})
+
+app.get("/admin/deleteUser",(req,res) => {
+    // logc of checking if the request is authenticated 
+
+    res.send("deleted a user")
 })
 
 app.listen(3000,() => {
