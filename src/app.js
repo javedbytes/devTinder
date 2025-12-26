758b1e92117eddf59e137b87c3c2a1116f840a27
Javed Ahmed <javed@Javeds-MacBook-Air.local>
2025-12-26 21:59:32 +0530

    get post delete api

--- src/app.js
+++ src/app.js
@@ -1,20 +1,44 @@
-const express = require('express');
+const express = require ('express');
 
 const app = express();
 
+// app.use("/",(req,res) => {
+//     res.send("hi from home page");
+// })
 
-app.use("/",(req, res) => {
-    res.send('Hello, homepage from server');
+// order or routing is very important
+
+// app.use("/hello",(req,res) => {
+//     res.send("hello hello hello");
+// })
+
+
+// this will match all the http api method to /test
+app.use("/test",(req,res) => {
+    res.send("hello from server");
 })
 
-app.use("/test",(req, res) => {
-    res.send('hello hello hello ');
+// only match get api call
+app.get("/user",(req,res) => {
+    res.send({firstName:"javed",lastName:"Ahmed"})
+
 })
 
-app.use("/hello",(req, res) => {
-    res.send('hello hello hello ');
+app.post("/user",(req,res) => {
+    console.log("save data to database")
+    res.send("data succesfully saved to DB")
+
 })
+
+app.delete("/user",(req,res) => {
+    res.send("deleted succesfully")
+
+})
+
+// app.use("/",(req,res) => {
+//     res.send("hi from home page");
+// })
 
 app.listen(3000,() => {
-    console.log('Server is running on port 3000');
+    console.log("server started on port 3000")
 });
