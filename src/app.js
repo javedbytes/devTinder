d1fa4fa23bd92ef1c063580ddbfdcdb889d04a81
Javed Ahmed <javed@Javeds-MacBook-Air.local>
2025-12-27 14:27:34 +0530

    db connection

--- src/app.js
+++ src/app.js
@@ -1,29 +1,17 @@
 const express = require ('express');
-
+const connectDB = require("./config/database")
 const app = express();
 
 
-app.get("/getUserData",(req,res) => {
-    // better to use try catch
-    try {
-        
-    } catch (error) {
-        
-    }
-    // logic of DB call to get user data
-    throw new Error("error")
-    res.send("user data send")
-})
-
-// error should be first parameter
-app.use("/",(error,req, res,next) => {
- if(error){
-    // log your error
-    res.status(500).send("something went wrong")
- }
-})
-
-
-app.listen(3000,() => {
+connectDB().then(() => {
+    console.log("Database connected")
+    app.listen(3000,() => {
     console.log("server started on port 3000")
 });
+}).catch( err => {
+    console.log("database connection fail")
+});
+
+//   app.listen(3000,() => {
+//     console.log("server started on port 3000")
+// });
