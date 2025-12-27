5c1df01e97b7bf5caf4c098376e94df819c0af66
javedbytes <javed.ahmed@akasaair.com>
2025-12-27 15:17:23 +0530

    created signup api and added to db

--- src/app.js
+++ src/app.js
@@ -1,7 +1,21 @@
 const express = require ('express');
 const connectDB = require("./config/database")
 const app = express();
+const User = require("./models/user")
 
+app.post("/signup", async (req, res) => {
+    const userObject = {
+        firstName : "abdullah",
+        lastName : "Ahmed",
+        emailId : "abdul@akasaair.com",
+        password: "abdul@123"
+    }
+
+    const user = new User(userObject) // new instance of the user model
+
+   await user.save()
+   res.send("user added succesfully")
+})
 
 connectDB().then(() => {
     console.log("Database connected")
@@ -11,7 +25,3 @@ connectDB().then(() => {
 }).catch( err => {
     console.log("database connection fail")
 });
-
-//   app.listen(3000,() => {
-//     console.log("server started on port 3000")
-// });
